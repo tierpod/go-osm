51f907f6b82151b5eddb96ff965187b6ce616971
Pavel Podkorytov <pod.pavel@gmail.com>
2018-01-19 10:46:49 +0500

    Add Polygon and Region in point package

---
+++ point/region_test.go
@@ -0,0 +1,56 @@
+package point
+
+import (
+	"testing"
+)
+
+func TestRegionContains(t *testing.T) {
+	polygon1 := Polygon{
+		LatLong{Lat: 10.0, Long: 0.0},
+		LatLong{Lat: 0.0, Long: 0.0},
+		LatLong{Lat: 0.0, Long: 5.0},
+		LatLong{Lat: 10.0, Long: 5.0},
+		LatLong{Lat: 10.0, Long: 0.0},
+	}
+
+	polygon2 := Polygon{
+		LatLong{Lat: 10.0, Long: 5.0},
+		LatLong{Lat: 0.0, Long: 5.0},
+		LatLong{Lat: 0.0, Long: 10.0},
+		LatLong{Lat: 10.0, Long: 10.0},
+		LatLong{Lat: 10.0, Long: 5.0},
+	}
+
+	region := Region{polygon1, polygon2}
+
+	testData := []struct {
+		ll     LatLong
+		result bool
+	}{
+		// inside
+		{
+			LatLong{Lat: 3.0, Long: 9.0},
+			true,
+		},
+		{
+			LatLong{Lat: 6.0, Long: 8.0},
+			true,
+		},
+		// outside
+		{
+			LatLong{Lat: 15.0, Long: 15.0},
+			false,
+		},
+		{
+			LatLong{Lat: -1.0, Long: -1.0},
+			false,
+		},
+	}
+
+	for _, tt := range testData {
+		result := region.Contains(tt.ll)
+		if result != tt.result {
+			t.Errorf("Polygon.Contains: point: %v: excepted %v, got %v", tt.ll, tt.result, result)
+		}
+	}
+}
