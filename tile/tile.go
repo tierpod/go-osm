4dd2ce188aad494bc99c41af4f84e80ba611b93c
Pavel Podkorytov <pod.pavel@gmail.com>
2018-01-19 10:49:16 +0500

    Remove NewFromLatLong: use subpackage point instead

--- tile/tile.go
+++ tile/tile.go
@@ -4,7 +4,6 @@ package tile
 
 import (
 	"fmt"
-	"math"
 	"path"
 	"regexp"
 	"strconv"
@@ -65,17 +64,4 @@ func NewFromURL(url string) (Tile, error) {
 		Y:     y,
 		Ext:   items[5],
 	}, nil
-}
-
-// NewFromLatLong creates Tile from latitude and longtitude coordinates.
-func NewFromLatLong(lat, long float64, zoom int) Tile {
-	var x, y int
-	x = int(math.Floor((long + 180.0) / 360.0 * (math.Exp2(float64(zoom)))))
-	y = int(math.Floor((1.0 - math.Log(math.Tan(lat*math.Pi/180.0)+1.0/math.Cos(lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(zoom)))))
-
-	return Tile{
-		Zoom: zoom,
-		X:    x,
-		Y:    y,
-	}
 }
